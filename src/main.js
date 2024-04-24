71690ae5618b5dd0d0ea812cbf8b2caf36b1a6a2
Matheus Lopes <inolopesm@gmail.com>
2024-04-24 11:31:07 -0300

    refactor: adds components dynamically

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { readFileSync } from "node:fs";
+import { readdirSync, readFileSync, statSync } from "node:fs";
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { parse } from "postcss";
@@ -7,24 +7,10 @@ import { objectify } from "postcss-js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-/**
- * Get Components
- * @param {string} name
- * @returns {import("postcss-js").CssInJs}
- */
-function getComponents(name) {
-  const css = readFileSync(`${__dirname}/${name}/${name}.css`, "utf8");
-  return objectify(parse(css));
-}
-
-/**
- * Setup edufit ui
- * @returns {import("tailwindcss/types/config").PluginCreator}
- */
 export default function edufit() {
-  return ({ addComponents }) => {
-    addComponents(getComponents("alert"));
-    addComponents(getComponents("button"));
-    addComponents(getComponents("input"));
-  };
+  return ({ addComponents }) =>
+    readdirSync(__dirname)
+      .filter((value) => statSync(`${__dirname}/${value}`).isDirectory())
+      .map((value) => readFileSync(`${__dirname}/${value}/${value}.css`))
+      .forEach((value) => addComponents(objectify(parse(value))));
 }
