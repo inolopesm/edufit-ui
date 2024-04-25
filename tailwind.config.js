6ff058544264fd75844e05cdbcedcc2ae8f63c09
Matheus Lopes <inolopesm@gmail.com>
2024-04-25 20:06:15 -0300

    feat: roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme.js";
 import edufit from "./src/main.js";
 
 /** @type {import("tailwindcss").Config} */
 export default {
   content: ["./src/**/*.html"],
-  theme: { extend: {} },
   plugins: [edufit()],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Roboto"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 };
